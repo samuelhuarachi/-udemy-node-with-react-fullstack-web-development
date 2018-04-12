2deb77a34a5023e624a5a5a19eef502c13198f30
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2018-04-12 20:01:27 -0300

    correcao do codigo

--- index.js
+++ index.js
@@ -12,23 +12,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-console.log(PORT);
-
 app.listen(PORT);
-
-const user = {
-    age: 20,
-    height: 1
-}
-
-Object.freeze(user);
-
-var user2 = Object.assign({
-    ... user,
-    age: 30,
-    height: 1.79
-}, user);
-
-console.log(user);
-
-console.log(user2.height);
